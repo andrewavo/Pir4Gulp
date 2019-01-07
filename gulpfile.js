3d0872e1f54eae31942a15a82ccca118f9b8ae8e
Orekhov <andrei.orekhov@paritet.ru>
2019-01-07 13:28:30 +0300

    Update build system

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ const imagemin = require('gulp-imagemin');
 const injectSvg = require('gulp-inject-svg');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
+const cssimport = require('gulp-cssimport');
+const debug = require('gulp-debug');
+const sourcemap = require('gulp-sourcemaps');
+const del = require('del');
 
 /* Test Gulp is running */
 gulp.task('message', function () {
@@ -13,7 +17,7 @@ gulp.task('message', function () {
 
 /* Clean before build */
 gulp.task('clean', function () {
-
+    return del('dist');
 });
 
 /* Inject svg images into html <img> and copy html files into dist*/
@@ -39,5 +43,13 @@ gulp.task('minifyjs', function () {
 
 /* Assemble css - resolve imports, build sourcemap */
 gulp.task('styles', function () {
-
+    return gulp.src('src/css/main.css')
+        .pipe(sourcemap.init())
+        .pipe(cssimport())
+        .pipe(sourcemap.write())
+        .pipe(gulp.dest('dist/css/'));
 });
+
+gulp.task('build', gulp.series(
+       'clean',
+       gulp.parallel('inject-svg', 'imagemin', 'minifyjs', 'styles')));
